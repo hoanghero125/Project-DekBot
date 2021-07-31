24fd29aafc0a8866e62f63199fc8436d81c6d377
Do Pham Bao Hoang <franlkinclinton@gmail.com>
2021-07-31 10:01:14 +0700

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -1,15 +1,16 @@
 const ytdl = require('ytdl-core');
 const ytSearch = require('yt-search');
+const util = require('minecraft-server-util');
 
 const queue = new Map();
 
 module.exports = {
     name: 'play',
-    aliases: ['skip', 'stop'],
+    aliases: ['skip', 'stop', 'mcsv', 'youtube'],
     cooldown: 0,
     description: 'Advanced music bot',
     async execute(message,args, cmd, client, Discord){
-
+        
         const voice_channel = message.member.voice.channel;
         if (!voice_channel) return message.channel.send('You must already in a channel to use this command!');
 
@@ -64,8 +65,34 @@ module.exports = {
 
         else if(cmd === 'skip') skip_song(message, server_queue);
         else if(cmd === 'stop') stop_song(message, server_queue);
+        else if(cmd === 'mcsv'){
+            if(!args[0]) return message.channel.send("Please enter a Minecraft's server ip");
+            if(!args[1]) return message.channel.send("Please enter a Minecraft's server port");
+     
+            util.status(args[0], {port: parseInt(args[1])}).then((response) =>{
+                console.log(response);
+                const embed = new Discord.MessageEmbed()
+                .setColor('#4E5D94')
+                .setTitle('Mc server status')
+                .addFields(
+                    {name: 'Server IP', value: response.host},
+                    {name: 'Online Players', value: response.onlinePlayers},
+                    {name: 'Max Players', value: response.maxPlayers},
+                    {name: 'Version', value: response.version}
+                )
+                .setFooter("Minecraft's server status by Dek");
+     
+                message.channel.send(embed);
+            })
+            .catch ((error) =>{
+                message.channel.send('There was an error finding this server');
+                throw error;
+            });
+        }
+        else if(cmd === 'youtube'){
+            message.channel.send("Here is my creator's Youtube channel: https://www.youtube.com/c/DekuranVN | Go subscribe (☞ﾟヮﾟ)☞")
+        }
     }
-    
 }
 
 const video_player = async (guild, song) => {
